6edb78f9e5906c5eec9194d2f67e296950cc6934
Jay Patel <jaypatel@Jays-MacBook-Pro-2.local>
2024-04-19 12:15:06 -0500

    updated meta

--- components/Metadata.vue
+++ components/Metadata.vue
@@ -4,6 +4,7 @@
     <Meta property="twitter:card" content="summary_large_image" />
     <Meta property="twitter:domain" content="coupleofpatels.com" />
     <Meta property="twitter:url" content="https://www.coupleofpatels.com/" />
+    <Meta property="twitter:image" content="https://www.coupleofpatels.com/logo.png" />
     <Meta
       property="twitter:image"
       content="https://www.coupleofpatels.com/logo.png"
