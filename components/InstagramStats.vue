1ce2574b7069ea0a2bfe87c18f630642749a90c7
Jay Patel <jaypatel@Jays-MacBook-Pro-2.local>
2024-04-23 17:03:26 -0500

    updated stats

--- components/InstagramStats.vue
+++ components/InstagramStats.vue
@@ -22,8 +22,8 @@
 </template>
 <script setup>
 const stats = [
-  { id: 1, name: "Instagram Followers", value: "13,500" },
-  { id: 2, name: "Posts", value: "125" },
+  { id: 1, name: "Instagram Followers", value: "14,500" },
+  { id: 2, name: "Posts", value: "130" },
   { id: 3, name: "Followers From Midwest", value: "90%" },
   { id: 4, name: "Engagement", value: "5.8%" },
 ];
